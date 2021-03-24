820a68f9b460fc26ec312e17c9a234d7d637a3b1
Tarek Belkahia <tarek.belkahia@360learning.com>
2021-03-24 10:38:51 +0100

    Use import alias

--- common/compose-ui/src/desktopMain/kotlin/com/github/tokou/common/platform/Scrollbar.kt
+++ common/compose-ui/src/desktopMain/kotlin/com/github/tokou/common/platform/Scrollbar.kt
@@ -6,10 +6,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.ScrollbarAdapter as ComposeScrollbarAdapter
+import androidx.compose.foundation.VerticalScrollbar as ComposeVerticalScrollbar
+import androidx.compose.foundation.rememberScrollbarAdapter as composeRememberScrollbarAdapter
 
 actual val MARGIN_SCROLLBAR: Dp = 8.dp
 
-actual typealias ScrollbarAdapter = androidx.compose.foundation.ScrollbarAdapter
+actual typealias ScrollbarAdapter = ComposeScrollbarAdapter
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -18,7 +21,7 @@ actual fun rememberScrollbarAdapter(
     itemCount: Int,
     averageItemSize: Dp
 ): ScrollbarAdapter =
-    androidx.compose.foundation.rememberScrollbarAdapter(
+    composeRememberScrollbarAdapter(
         scrollState = scrollState,
         itemCount = itemCount,
         averageItemSize = averageItemSize
@@ -29,7 +32,7 @@ actual fun VerticalScrollbar(
     modifier: Modifier,
     adapter: ScrollbarAdapter
 ) {
-    androidx.compose.foundation.VerticalScrollbar(
+    ComposeVerticalScrollbar(
         modifier = modifier,
         adapter = adapter
     )
