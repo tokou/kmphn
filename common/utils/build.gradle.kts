6b2075fa6fa294cfda0bc66b0b0b2e8525f1a9f4
Tarek Belkahia <tarek.belkahia@360learning.com>
2021-03-24 10:41:44 +0100

    Add utils

--- common/utils/build.gradle.kts
+++ common/utils/build.gradle.kts
@@ -38,6 +38,8 @@ kotlin {
             dependencies {
                 implementation("com.arkivanov.decompose:decompose:0.1.9")
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.3")
+                implementation("com.arkivanov.mvikotlin:mvikotlin:2.0.1")
+                implementation("com.arkivanov.mvikotlin:mvikotlin-extensions-coroutines:2.0.1")
             }
         }
         val commonTest by getting
---
+++ common/utils/src/commonMain/kotlin/com/github/tokou/common/utils/Store.kt
@@ -0,0 +1,31 @@
+package com.github.tokou.common.utils
+
+import com.arkivanov.decompose.value.Value
+import com.arkivanov.decompose.value.ValueObserver
+import com.arkivanov.mvikotlin.core.store.Store
+import com.arkivanov.mvikotlin.extensions.coroutines.states
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.onEach
+
+@OptIn(ExperimentalCoroutinesApi::class)
+fun <T : Any> Store<*, T, *>.asValue(coroutineScope: CoroutineScope): Value<T> =
+    object : Value<T>() {
+        override val value: T get() = state
+        private var jobs = emptyMap<ValueObserver<T>, Job>()
+
+        override fun subscribe(observer: ValueObserver<T>) {
+            val job = states
+                .onEach { observer(it) }
+                .launchIn(coroutineScope)
+            this.jobs += observer to job
+        }
+
+        override fun unsubscribe(observer: ValueObserver<T>) {
+            val job = jobs[observer] ?: return
+            this.jobs -= observer
+            job.cancel()
+        }
+    }
