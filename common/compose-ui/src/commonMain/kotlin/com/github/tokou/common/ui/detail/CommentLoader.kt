fc8bd6ff5a697126b656372d985c5044fcec9914
Tarek Belkahia <tarek.belkahia@360learning.com>
2021-03-27 09:21:18 +0100

    Switch comment loader

--- common/compose-ui/src/commonMain/kotlin/com/github/tokou/common/ui/detail/CommentLoader.kt
+++ common/compose-ui/src/commonMain/kotlin/com/github/tokou/common/ui/detail/CommentLoader.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -18,12 +19,22 @@ fun CommentLoader() {
             .fillMaxWidth()
             .height(48.dp)
     ) {
+        // Seems to cause jankiness, disable for now
+        /*
         CircularProgressIndicator(
             modifier = Modifier
                 .align(Alignment.CenterStart)
                 .padding(start = 16.dp)
                 .padding(vertical = 4.dp),
             color = MaterialTheme.colors.primary.copy(alpha = 0.2f)
+        )
+         */
+        Text(
+            text = "...",
+            modifier = Modifier
+                .align(Alignment.CenterStart)
+                .padding(start = 16.dp)
+                .padding(vertical = 4.dp)
         )
     }
 }
