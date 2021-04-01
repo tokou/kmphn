42f62a3d26bdbdbfc71d2239f67044cfd31df056
Tarek Belkahia <tarek.belkahia@gmail.com>
2021-04-01 19:50:03 +0200

    Add tuist project config

--- ios/Project.swift
+++ ios/Project.swift
@@ -1,10 +1,66 @@
 import ProjectDescription
-import ProjectDescriptionHelpers
 
 // MARK: - Project
 
-let project = Project.app(
-    name: "App",
-    platform: .iOS,
-    additionalTargets: []
+let name = "App"
+let platform: Platform = .iOS
+
+let frameworkPath = "../common/root/build/xcode-frameworks/Hackernews.framework"
+
+let script = """
+cd $SRCROOT/..
+./gradlew :common:root:packForXCode -PXCODE_CONFIGURATION=${CONFIGURATION}
+cd $SRCROOT
+"""
+
+let targetActions = [
+    TargetAction.pre(
+        script: script,
+        name: "Build Hackernews framework",
+        basedOnDependencyAnalysis: true
+    )
+]
+
+let infoPlist: [String: InfoPlist.Value] = [
+    "CFBundleShortVersionString": "1.0",
+    "CFBundleVersion": "1",
+    "UIMainStoryboardFile": "",
+    "UILaunchStoryboardName": "LaunchScreen"
+]
+
+let organizationName = "com.github.tokou"
+let bundleId = "\(organizationName).hackernews"
+
+let mainTarget = Target(
+    name: name,
+    platform: platform,
+    product: .app,
+    bundleId: bundleId,
+    infoPlist: .extendingDefault(with: infoPlist),
+    sources: ["Targets/\(name)/Sources/**"],
+    resources: ["Targets/\(name)/Resources/**"],
+    actions: targetActions,
+    dependencies: [
+        .framework(path: .relativeToRoot(frameworkPath))
+    ]
+)
+
+let testTarget = Target(
+    name: "\(name)Tests",
+    platform: platform,
+    product: .unitTests,
+    bundleId: "\(bundleId).tests",
+    infoPlist: .default,
+    sources: ["Targets/\(name)/Tests/**"],
+    dependencies: [
+        .target(name: "\(name)")
+    ]
+)
+
+let targets = [mainTarget, testTarget]
+
+let project = Project(
+    name: name,
+    organizationName: organizationName,
+    targets: targets
 )
