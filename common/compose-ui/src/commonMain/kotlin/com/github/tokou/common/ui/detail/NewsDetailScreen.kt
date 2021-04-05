1aaa9b029a400517b741c80facbfadf98e210181
Tarek Belkahia <tarek.belkahia@gmail.com>
2021-04-05 09:15:46 +0200

    Delete extra class

--- common/compose-ui/src/commonMain/kotlin/com/github/tokou/common/ui/detail/NewsDetailScreen.kt
+++ common/compose-ui/src/commonMain/kotlin/com/github/tokou/common/ui/detail/NewsDetailScreen.kt
@@ -1,20 +1,13 @@
 package com.github.tokou.common.ui.detail
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.arkivanov.decompose.extensions.compose.jetbrains.asState
 import com.github.tokou.common.detail.NewsDetail
 import com.github.tokou.common.detail.NewsDetail.Model
 import com.github.tokou.common.ui.main.ErrorLayout
+import com.github.tokou.common.ui.main.Loader
 
 @Composable
 fun NewsDetailScreen(modifier: Modifier = Modifier, component: NewsDetail) {
@@ -25,7 +18,7 @@ fun NewsDetailScreen(modifier: Modifier = Modifier, component: NewsDetail) {
         onBack = component::onBack,
     ) {
         when (val m = model) {
-            Model.Loading -> Loading()
+            Model.Loading -> Loader()
             Model.Error -> ErrorLayout(text = "Error", onClick = component::onRetry)
             is Model.Content -> NewsDetailContent(
                 content = m,
@@ -34,12 +27,5 @@ fun NewsDetailScreen(modifier: Modifier = Modifier, component: NewsDetail) {
                 onLinkClicked = component::onLinkClicked,
             )
         }
-    }
-}
-
-@Composable
-private fun Loading() {
-    Box(modifier = Modifier.fillMaxSize()) {
-        CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
     }
 }
