edd250b42b05a38cc15a69dd810937a3714eb090
Tarek Belkahia <tarek.belkahia@gmail.com>
2021-04-10 09:56:28 +0200

    Switch RichTextView implementation

--- ios/Targets/App/Sources/Views/Common/RichTextView.swift
+++ ios/Targets/App/Sources/Views/Common/RichTextView.swift
@@ -1,148 +1,53 @@
 import SwiftUI
 import Hackernews
 
-class TapabbleLabel: UILabel {
-
-    let layoutManager = NSLayoutManager()
-    let textContainer = NSTextContainer(size: CGSize.zero)
-    var textStorage = NSTextStorage() {
-        didSet {
-            textStorage.addLayoutManager(layoutManager)
-        }
-    }
-
-    var onLinkTapped: (URL) -> () = { _ in }
-
-    let tapGesture = UITapGestureRecognizer()
-
-    override var attributedText: NSAttributedString? {
-        didSet {
-            if let attributedText = attributedText {
-                textStorage = NSTextStorage(attributedString: attributedText)
-            } else {
-                textStorage = NSTextStorage()
-            }
-        }
-    }
-
-    override var lineBreakMode: NSLineBreakMode {
-        didSet {
-            textContainer.lineBreakMode = lineBreakMode
-        }
-    }
-
-    override var numberOfLines: Int {
-        didSet {
-            textContainer.maximumNumberOfLines = numberOfLines
-        }
-    }
-
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        setUp()
-    }
-
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setUp()
-    }
-
-    func setUp() {
-        isUserInteractionEnabled = true
-        layoutManager.addTextContainer(textContainer)
-        textContainer.lineFragmentPadding = 0
-        textContainer.lineBreakMode = lineBreakMode
-        textContainer.maximumNumberOfLines = numberOfLines
-        tapGesture.addTarget(self, action: #selector(labelTapped))
-        addGestureRecognizer(tapGesture)
-    }
-
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        textContainer.size = bounds.size
-    }
-
-    @objc func labelTapped() {
-        guard tapGesture.state == .ended else {
-            return
-        }
-
-        let locationOfTouch = tapGesture.location(in: tapGesture.view)
-        let textBoundingBox = layoutManager.usedRect(for: textContainer)
-        let textContainerOffset = CGPoint(
-            x: (bounds.width - textBoundingBox.width) / 2 - textBoundingBox.minX,
-            y: (bounds.height - textBoundingBox.height) / 2 - textBoundingBox.minY
-        )
-        let locationOfTouchInTextContainer = CGPoint(
-            x: locationOfTouch.x - textContainerOffset.x,
-            y: locationOfTouch.y - textContainerOffset.y
-        )
-        let indexOfCharacter = layoutManager.characterIndex(
-            for: locationOfTouchInTextContainer,
-            in: textContainer,
-            fractionOfDistanceBetweenInsertionPoints: nil
-        )
-        onCharacterTapped(label: self, characterIndex: indexOfCharacter)
-    }
-    
-    func onCharacterTapped(label: UILabel, characterIndex: Int) {
-        if
-            let attribute = label.attributedText?.attribute(NSAttributedString.Key.link, at: characterIndex, effectiveRange: nil) as? String,
-            let url = URL(string: attribute)
-        {
-            self.onLinkTapped(url)
-        }
-    }
+extension NewsDetailText: Identifiable {
+    public var id: String { text + String(describing: self) }
 }
 
-struct RichTextView: UIViewRepresentable {
+struct RichTextView : View {
     let text: [NewsDetailText]
     let onLinkClicked: (String, Bool) -> ()
 
-    func makeUIView(context: UIViewRepresentableContext<Self>) -> UILabel {
-        let label = TapabbleLabel()
-        label.lineBreakMode = .byWordWrapping
-        label.numberOfLines = 0
-        label.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
-        label.setContentHuggingPriority(.defaultHigh, for: .vertical)
-        label.onLinkTapped = { url in onLinkClicked(url.absoluteString, false) }
-        return label
-    }
-
-    private func buildAttributedString() -> NSAttributedString {
-        let attributedString = NSMutableAttributedString()
-        for t in text {
+    var body: some View {
+        text.map { t in
             switch t {
             case let t as NewsDetailText.Plain:
-                attributedString.append(.init(string: t.text, attributes: [.font: UIFont.preferredFont(forTextStyle: .callout)]))
+                return Text(t.text)
             case let t as NewsDetailText.Emphasis:
-                attributedString.append(.init(string: t.text, attributes: [.font: UIFont.preferredFont(forTextStyle: .callout), .obliqueness: 0.2]))
+                return Text(t.text)
+                    .italic()
             case let t as NewsDetailText.Code:
-                attributedString.append(.init(string: t.text, attributes: [.font: UIFont.monospacedSystemFont(ofSize: 16, weight: .regular)]))
+                return Text(t.text)
+                    .font(.system(.callout, design: .monospaced))
             case let t as NewsDetailText.Link:
-                attributedString.append(.init(string: t.text, attributes: [.link: t.link, .font: UIFont.preferredFont(forTextStyle: .callout)]))
+                return Text(t.text)
+                    .underline()
+                    .foregroundColor(.blue)
             default: fatalError("Unkown span type")
             }
-        }
-        return attributedString
-    }
-        
-    func updateUIView(_ uiView: UILabel, context: Context) {
-        uiView.attributedText = buildAttributedString()
+        }.reduce(Text("")) { result, text in result + text }
     }
 }
 
 struct RichText_Previews: PreviewProvider {
+    static let stubText = [
+        NewsDetailText.Plain(text: "Hello there,\n"),
+        NewsDetailText.Emphasis(text: "Hello there,\n"),
+        NewsDetailText.Link(text: "https://www.spacejam.com/\n", link: "https://www.spacejam.com/"),
+        NewsDetailText.Code(text: "Hmm in teresting"),
+        NewsDetailText.Plain(text: "\nThe goal is to get a UILabel integrated via UIViewRepresentable to size the same way Text does - use the available width and wrap to multiple lines to fit all the text thus increasing the height of the HStack it's in, instead of expanding in width infinitely. This is very similar to this question, though the accepted answer does not work for the layout I'm using involving a ScrollView, VStack, and HStack.")
+    ]
+
     static var previews: some View {
         RichTextView(
-            text: [
-                NewsDetailText.Plain(text: "Hello there,\n"),
-                NewsDetailText.Emphasis(text: "Hello there,\n"),
-                NewsDetailText.Link(text: "https://www.spacejam.com/\n", link: "https://www.spacejam.com/"),
-                NewsDetailText.Code(text: "Hmm in teresting")
-            ],
+            text: stubText,
             onLinkClicked: { _, _ in }
         )
         .previewLayout(.sizeThatFits)
+        List {
+            RichTextView(text: stubText, onLinkClicked: { l, _ in print(l) })
+            RichTextView(text: stubText, onLinkClicked: { l, _ in print(l) })
+        }
     }
 }
